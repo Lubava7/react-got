8fe2958641dc29cdbe4c23cc978b549cdf4e9a62
lubava <joheunsaram@mail.ru>
2022-02-01 20:22:20 +0300

    add a copy file of Time component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 // import StudentComp from './compoent/Student/Student';
-// import ReactTryTime from './compoent/ReactTryTime/ReactTryTime';
+import ReactTryTime from './compoent/ReactTryTime/ReactTryTime';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       {/* <StudentComp/> */}
-      {/* <ReactTryTime/> */}
+      <ReactTryTime/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

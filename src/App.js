84c887941004baa05379969607b3c46d09fce9d7
lubava <joheunsaram@mail.ru>
2022-02-01 18:31:20 +0300

    edit studet comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import StudentComp from './compoent/Student/Student';
+// import StudentComp from './compoent/Student/Student';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      {/* <StudentComp/> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <StudentComp/>
+      
     </div>
   );
 }
--- src/compoent/Student/Student.js
+++ src/compoent/Student/Student.js
@@ -1,19 +1,24 @@
 import React from "react"
 import "./Student.css"
 
-
+// на каждый элемент массива вернуть <p> чтобы получлся список
+    
 
 const students = [ 
-    {name: 'John', id: 123, marks : 98 }, 
-    {name: 'Baba', id: 101, marks : 23 }, 
-    {name: 'yaga', id: 200, marks : 45 }, 
-    {name: 'Wick', id: 115, marks : 75 } 
-    ]
-function StudentComp (students){    
-const liStudents = students.map((student) => {
-    return <li key={student.toString()}>{student}</li>
-}); 
-  return <ul>{liStudents}</ul>
+  {name: 'John', id: 123, marks : 98 }, 
+  {name: 'Baba', id: 101, marks : 23 }, 
+  {name: 'yaga', id: 200, marks : 45 }, 
+  {name: 'Wick', id: 115, marks : 75 } 
+  ]
+function StudentComp (props){    
+
+let liStudents = students.map((student)=> 
+<li>
+  {student.name}, {student.id}, {student.marks}
+</li>);
+
+return <ul>{liStudents}</ul>
+
 }
 
 

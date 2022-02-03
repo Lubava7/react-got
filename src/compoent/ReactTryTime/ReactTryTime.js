b0b181466bcbe5f13120d83aa104466cf50f33fd
lubava <joheunsaram@mail.ru>
2022-02-03 15:31:28 +0300

    finish the clock with second auto re fresh component

--- src/compoent/ReactTryTime/ReactTryTime.js
+++ src/compoent/ReactTryTime/ReactTryTime.js
@@ -1,14 +1,57 @@
 import React from "react"
 import "./ReactTryTime.css"
 
-function ReactTryTime(props){
-    return(
-<div>
-    <p>Hello, everyone!</p>
-    <p>Current data {new Date().toLocaleDateString()}.</p>
-    <p>Current time {new Date().toLocaleTimeString()}.</p>
-</div>
-    )}
-setInterval(ReactTryTime, 1000);
+// function ReactTryTime(props){
+//     return(
+// <div>
+//     <p>Hello, everyone!</p>
+//     <p>Current data {props.date.toLocaleDateString()}.</p>
+//     <p>Current time {props.date.toLocaleTimeString()}.</p>
+// </div>
+//     )}
+// // setInterval(ReactTryTime, 1000);
+function tick(){
+    // const time = (
+    //     <div>
+    //     <p>Hello everyone!</p>
+    //     <p>Current time {this.state.date.toLocaleTimeString()}.</p>
+    // </div>
+
+    // )
+    // this.setState({
+    //     date: new Date()
+    // })
+}
+setInterval( tick, 1000)
+
+class ReactTryTime extends React.Component{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         date: new Date()
+      }
+    }
+    componentDidMount(){
+        this.timerID = setInterval(()=> this.tick(), 1000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({
+            date : new Date()
+        })
+    }
+    render(){
+        return(
+             <div>
+                 <p>Hello everyone!</p>
+                 <p>Current time {this.state.date.toLocaleTimeString()}.</p>
+             </div>
+        )
+    }
+    
+}
 
 export default ReactTryTime;

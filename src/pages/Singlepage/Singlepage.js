58cf98ae83749c83e0d6c5a41205350f27706cb7
lubava <joheunsaram@mail.ru>
2022-03-05 02:35:14 +0300

    edit route and singlepage

--- src/pages/Singlepage/Singlepage.js
+++ src/pages/Singlepage/Singlepage.js
@@ -3,28 +3,25 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 function Singlepage() {
-  const { index } = useParams();
+  const { aliases } = useParams();
   const [item, setItem] = useState(null);
 
   useEffect(() => {
-    fetch(`https://anapioficeandfire.com/api/characters/${index}`)
+    fetch(`https://anapioficeandfire.com/api/characters/${aliases}`)
       .then((res) => res.json())
       .then((value) => setItem(value));
-  }, [index]);
+  }, [aliases]);
 
   return (
     <div>
       hhfhfhfhfh
-      {
-        (item,
-        index && (
-          <div>
-            <h1>{item.aliases}</h1>
-            <p>{item.gender}</p>
-            <Link to={`/items`}>back link</Link>
-          </div>
-        ))
-      }
+      {item && (
+        <div>
+          <h1>{item.aliases}</h1>
+          <p>{item.gender}</p>
+          <Link to={`/items`}>back link</Link>
+        </div>
+      )}
     </div>
   );
 }

774bb935ba619513c6cc538dd99d2b5f5007a8ee
lubava <joheunsaram@mail.ru>
2022-03-15 01:03:26 +0300

    add abasename in index js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import AppTwo from "./AppTwo";
-// import AppThree from "./AppThree";
+
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+console.log(process.env.PUBLIC_URL);
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
-      {/* <AppTwo />
-    <AppThree /> */}
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
